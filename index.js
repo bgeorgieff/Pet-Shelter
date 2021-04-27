e03b20e3e86a533d28b7b523053554778c08cff4
Blagovest Georgiev <b.g.georgiev87@gmail.com>
2021-04-27 16:57:27 +0300

    feat: installed dep and basic routing plus refactoring

---
+++ index.js
@@ -0,0 +1,27 @@
+require('dotenv').config()
+const env = process.env.NODE_ENV || 'development'
+
+const config = require('./config/config')[env]
+const express = require('express')
+const indexRouter = require('./routes')
+const app = express()
+
+const mongoose = require('mongoose')
+
+mongoose.connect(config.dataBaseUrl, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+} ,(err) => {
+    if (err) {
+        console.error('error', err)
+        throw err
+    }
+
+    console.log('Data base is connected');
+})
+
+require('./config/express')(app)
+
+app.use('/', indexRouter)
+
+app.listen(config.port, console.log(`Listening on port ${config.port}! Now its up to you...`))
